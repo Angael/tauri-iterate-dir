4048b54e0a2d8e15d82d8e59ceffb737959d1885
Angael <krzysztofwidacki@gmail.com>
2024-02-09 23:27:00 +0100

    Delete unused files and folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
-
-const title = "React";
+import { useQuery } from "@tanstack/react-query";
+import { TextInput, Title } from "@mantine/core";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [text, setText] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["directory", text],
+    queryFn: async () => {
+      return (await invoke("greet", { name: text })) as string;
+    }
+  });
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // TODO
+  };
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <Title>Iterate over a directory</Title>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+      <form className="row" onSubmit={onSubmit}>
+        <TextInput
+          label="Folder"
+          value={text}
+          onChange={(event) => setText(event.currentTarget.value)}
         />
         <button type="submit">Greet</button>
       </form>
 
-      <p>{greetMsg}</p>
+      <pre>{isLoading ? "Loading..." : isError ? "Error" : data}</pre>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MantineProvider, createTheme } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+const queryClient = new QueryClient();
+
+// Your theme configuration is merged with default theme
+const theme = createTheme({
+  fontFamily: "Montserrat, sans-serif",
+  defaultRadius: "md"
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme} defaultColorScheme="dark">
+        <App />
+      </MantineProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
