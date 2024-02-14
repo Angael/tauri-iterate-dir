4c5ba1329e6afaac6710ba4d35e64191d90eadd1
Angael <krzysztofwidacki@gmail.com>
2024-02-14 18:48:22 +0100

    esnext config; icons; sorting files

--- src/components/file-list/FileList.tsx
+++ src/components/file-list/FileList.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "@mantine/core";
 import type { File } from "./File.type";
+import { IconFolder, IconFile } from "@tabler/icons-react";
 
 type Props = {
   paths: File[];
@@ -7,12 +8,24 @@ type Props = {
 };
 
 const FileList = ({ paths, onClickPath }: Props) => {
-  return paths.map((file) => (
+  const sortedPaths = paths.toSorted((a, b) => {
+    if (a.isDir && !b.isDir) {
+      return -1;
+    }
+    if (!a.isDir && b.isDir) {
+      return 1;
+    }
+    return a.name.localeCompare(b.name);
+  });
+
+  return sortedPaths.map((file) => (
     <NavLink
       key={file.name}
       onClick={() => onClickPath(file.name)}
       label={file.name}
-      c={file.isDir ? "blue" : "gray"}
+      c={file.isDir ? "yellow.1" : "gray"}
+      style={{}}
+      leftSection={file.isDir ? <IconFolder /> : <IconFile />}
     />
   ));
 };
